c144448ebaf67005b2350ef3bc5064770e75fcca
dibu28 <dibu28@gmail.com>
2015-12-28 16:27:19 +0600

    Moved yandex disk backend to the separate project at github.com/dibu28/yandex

--- yandex/api/files_resource_list.go
+++
@@ -1,7 +0,0 @@
-package src
-
-type FilesResourceListResponse struct {
-	Items  []ResourceInfoResponse `json:"items"`
-	Limit  *uint64                `json:"limit"`
-	Offset *uint64                `json:"offset"`
-}
--- yandex/api/last_uploaded_resource_list.go
+++
@@ -1,6 +0,0 @@
-package src
-
-type LastUploadedResourceListResponse struct {
-	Items []ResourceInfoResponse `json:"items"`
-	Limit *uint64                `json:"limit"`
-}
